44ddd4d9a5984e1fa2c7cbd4e2557a55f242ac11
Giacomo Oricco <g.oricco.3585@vallauri.edu>
2025-12-24 11:38:48 +0100

    End Logic

--- index.js
+++ index.js
@@ -1,31 +1,80 @@
 ﻿"use strict";
-	
-let tbody = document.querySelector("tbody")
-let button = document.querySelector("button")
-button.addEventListener("click", fillTable)
 
-function fillTable(){
+let people = [];
+let people_ix = -1;
+
+let button = document.querySelector("button")
+let slider = document.getElementById("PeopleSlider");
+let radiosGenre = document.getElementsByName("chkGenere");
+let nats = document.getElementsByClassName("chkNation");
+let lblCorr = document.getElementById("lblCorr");
+let lblTot = document.getElementById("lblTot");
+let btnIndietro = document.getElementById("btnIndietro");
+let btnAvanti = document.getElementById("btnAvanti");
+button.addEventListener("click", loadUsers)
+
+function loadUsers(){
+
+    let gender = checkRadios();
+    let natlist = [];
+    natlist = getnats();
+    natlist = natlist.join(",");
+
+
     let params = {
-        results:20,
-        gender:"male"
+        results: slider.value,
+        gender: gender,
+        nat: natlist,
     }
                                 // metodo, risorsa, parametri
     let promise = ajax.sendRequest("GET", "/api", params)
     promise.catch(ajax.errore)
     promise.then(function(httpResponse){
-       console.log(httpResponse)
-       let people = httpResponse.data;
-       tbody.innerHTML=""
-       for (let person of people.results){
-           let tr = document.createElement("tr") 
-           tbody.appendChild(tr)
-
-           let td = document.createElement("td") 
-           td.textContent = person.name.first +  " " + person.name.last
-           tr.appendChild(td)
+       people = httpResponse.data.results;
+       if(people) {
+            console.log(people);
+            people_ix = 0;
+            lblTot.innerHTML = people.length;
+            btnIndietro.removeAttribute("disabled");
+            btnAvanti.removeAttribute("disabled");
+            loadPerson(people_ix);
+       } else {
+            alert("Ci è stato un errore.");
        }
-
     })
 }
 
+function loadPerson(index) {
+    // Qua carico tutte le info della persona
+    const person = people[index];
+    if(!person) {
+        alert("Elenco finito!");
+        return;
+    }
 
+    lblCorr.innerHTML = people_ix+1;
+}
+
+function checkRadios()
+{
+    for(let i = 0;i<radiosGenre.length;i++)
+    {
+        if(radiosGenre[i].checked)
+        {
+            return radiosGenre[i].value;
+        }
+    }
+}
+
+function getnats()
+{
+    let temp = [];
+    for(let i = 0;i<nats.length;i++)
+    {
+        if(nats[i].checked)
+        {
+            temp.push(nats[i].value.toLowerCase());
+        }
+    }
+    return temp;
+}
